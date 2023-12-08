e22221f42dd0b0396742b9348aa354823c6986a0
Tristantwj <tristan7an@gmail.com>
2023-12-08 22:41:22 +0800

    Add project files.

---
+++ Swaperoo_PE01_Grp9/Server/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Swaperoo_PE01_Grp9.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
