6a829ea89f8f51e9e5091b985c8cd14739202095
Tristantwj <tristan7an@gmail.com>
2024-01-17 09:09:05 +0800

    ss

---
+++ Swaperoo_PE01_Grp9/Client/Static/Endpoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Swaperoo_PE01_Grp9.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string ProductsEndpoint = $"{Prefix}/Products";
+
+    }
+}
